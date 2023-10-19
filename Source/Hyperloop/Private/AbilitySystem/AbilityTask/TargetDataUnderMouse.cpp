14307c38704113cdfc0351312aeae7e825ac95e5
pagon69 <30706246+pagon69@users.noreply.github.com>
2023-10-18 21:47:31 -0400

    collecting data from under mouse cursor for client and serverr at 10.7

--- Source/Hyperloop/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
+++ Source/Hyperloop/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
@@ -2,30 +2,65 @@
 
 
 #include "AbilitySystem/AbilityTask/TargetDataUnderMouse.h"
+#include "AbilitySystemComponent.h"
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
 
 	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
-
-
-
-
+	
 	return MyObj;
-
-	
-	
 	
 }
 
 void UTargetDataUnderMouse::Activate()
 {
 
-	 APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+
+	if(bIsLocallyControlled)
+	{
+		SendMouseCurserData();
+	}
+	else
+	{
+		//TODO: 
+	}
+	
+
+	
+}
+
+void UTargetDataUnderMouse::SendMouseCurserData()
+{
+
+	FScopedPredictionWindow ScopedPrediction(AbilitySystemComponent.Get());
+
+	//gets the target data under mouse and broadcast to client but need to handle server
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
 
 	FHitResult CursorHit;
 	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	
+	//ValidData.Broadcast(CursorHit.Location);
 
-	ValidData.Broadcast(CursorHit.Location);
+	FGameplayAbilityTargetDataHandle DataHandle;
+	
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = CursorHit;
+	DataHandle.Add(Data);
+
+	//sends mouse target data to the server
+	AbilitySystemComponent->ServerSetReplicatedTargetData(GetAbilitySpecHandle(),
+												 GetActivationPredictionKey(),
+												 DataHandle, FGameplayTag(),
+												 AbilitySystemComponent->ScopedPredictionKey);
+
+
+	if(ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);	
+	}
+
 	
 }
