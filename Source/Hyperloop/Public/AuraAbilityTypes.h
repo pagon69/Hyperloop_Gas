dc37cebaac2a03e1810f60d91ec1960c4360730d
pagon69 <30706246+pagon69@users.noreply.github.com>
2023-10-30 21:26:15 -0400

    net serialized code , dont understand but can avoid for now section 14 - 153

---
+++ Source/Hyperloop/Public/AuraAbilityTypes.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual UScriptStruct* GetScriptStruct() const override;
+	
+	//for how to send data over a network
+	virtual  bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
+
+	// inline getters
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	//inline setter
+	void SetIsCriticalHit (bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit (bool bInIsBlockedHit) {bIsBlockedHit = bInIsBlockedHit; }
+	
+	
+protected:
+
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+	
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+	
+
+	
+};
