09e6456240aa362746fca96da6c5361c4df46873
pagon69 <30706246+pagon69@users.noreply.github.com>
2023-11-13 20:29:30 -0500

    random spawn locations

--- Source/Hyperloop/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
+++ Source/Hyperloop/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -3,35 +3,38 @@
 
 #include "AbilitySystem/Abilities/AuraSummonAbility.h"
 
-#include "Kismet/KismetSystemLibrary.h"
+
 
 TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 {
 
-	const float DeltaSpread = SpawnSpreed /NumMinions;
-
-	
+	const float DeltaSpread = SpawnSpreed / NumMinions;
 	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector(); // forwrad vector
 	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation(); // actor location
 
 	//all posiable locatiosn for spawning
-	const FVector LeftOfSpread = Forward.RotateAngleAxis(SpawnSpreed / 2.f, FVector::UpVector); // a point infront to the left of actor
-	const FVector RightOfSpread = Forward.RotateAngleAxis(-SpawnSpreed / 2.f, FVector::UpVector); // a point in front to right of actor
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpreed / 2.f, FVector::UpVector); // a point infront to the left of actor
+	//const FVector RightOfSpread = Forward.RotateAngleAxis(SpawnSpreed / 2.f, FVector::UpVector); // a point in front to right of actor
 	
 	TArray<FVector> SpawnLocations;
 	
 	for (int32 i =0; i <NumMinions; i++)
 	{
 		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+		 FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+	//	DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 18.f, 12, FColor::Cyan, false, 3.f);
+	//	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + Direction * MaxSpawnDistance, 4.f, FLinearColor::Black, 3.f);
+	//	DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::Purple, false, 3.f);
+	//	DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::Purple, false, 3.f);
 
-		const FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
-		DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 18.f, 12, FColor::Cyan, false, 3.f);
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit, ChosenSpawnLocation + FVector(0.f ,0.f, 400.f), ChosenSpawnLocation - FVector(0.f, 0.f, 400.f), ECC_Visibility);
 
-		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + Direction * MaxSpawnDistance, 4.f, FLinearColor::Black, 3.f);
+		if(Hit.bBlockingHit)
+		{
+			ChosenSpawnLocation = Hit.ImpactPoint;
+		}
 		
-		DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::Purple, false, 3.f);
-		DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::Purple, false, 3.f);
-
 		SpawnLocations.AddUnique(ChosenSpawnLocation);
 	}
 	
